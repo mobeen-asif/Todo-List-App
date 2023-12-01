4fa782d3b4868a208f6c61b9968c745433eeb66c
Mobeen Asif <mobeenasif1901@gmail.com>
2023-12-01 11:21:22 +0500

    Done.

--- app.js
+++ app.js
@@ -1,23 +1,35 @@
-const input = document.querySelector('.add-task');
-const addBtn = document.querySelector('.add-btn');
-const tasks = document.querySelector('.tasks');
+const input = document.querySelector('#input-box');
+const addBtn = document.querySelector('#add-btn');
+const tasks = document.querySelector('.list-container');
 
-input.addEventListener('keyup', () => {
-    if (input.value.trim() !== 0) {
-        addBtn.classList.add('active')
-    } else {
-        addBtn.classList.remove('active')
-    }
-})
+// input.addEventListener('keyup', () => {
+//     if (input.value.trim() !== 0) {
+//         addBtn.classList.add('active')
+//     } else {
+//         addBtn.classList.remove('active')
+//     }
+// })
+{/* <i class="fa-solid fa-xmark" id="edit"></i> */}
+{/* <i class="fa-solid fa-pen-to-square" id="close"></i> */}
 
 addBtn.addEventListener('click', () => {
-    if(input.value.trim() !== 0){
-        let newItem = document.createElement('div');
-        newItem.classList.add('item');
+    if (input.value === '') {
+        alert('Plz type something')
+    } else {
+        let newItem = document.createElement('li');
         newItem.innerHTML = `<p> ${input.value}</p>
         <div class="item-btn">
-            <i class="fa-solid fa-pen-to-square"></i>
-            <i class="fa-solid fa-xmark"></i>
+            
+            <button id='close'>X</button>
         </div>`
+        tasks.appendChild(newItem);
+    }
+    input.value = '';
+});
+
+tasks.addEventListener('click', (e)=>{
+    if(e.target.value === 'X'){
+        e.target.parentElement.remove();
+    } else if (e.target.idName === 'edit'){
     }
 })
